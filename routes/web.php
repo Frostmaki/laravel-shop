0112f925ca046b25cca8e0d0510d5d7f31374999
vagrant <vagrant@homestead>
2018-06-18 07:10:30 +0000

    邮箱验证middleware

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,13 @@ Route::get('/','PagesController@root')->name('root');
 
 Auth::routes();
 
+Route::group(['middleware'=> 'auth'],function (){
+    Route::get('/emailVerifyNotice','PagesController@emailVerifyNotice')->name('emailVerifyNotice');
 
+    //email verify
+    Route::group(['middleware' => 'email_verified'],function (){
+        Route::get('/test',function (){
+            return 'Your email is verified';
+        });
+    });
+});
